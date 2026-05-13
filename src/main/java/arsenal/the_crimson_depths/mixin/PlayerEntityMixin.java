6831ed3dc78088cc7c0825b517c2e515f63dc777
VoidAtomicX <voidatomicx@gmail.com>
2026-05-12 20:43:59 -0600

    new

--- src/main/java/arsenal/the_crimson_depths/mixin/PlayerEntityMixin.java
+++ src/main/java/arsenal/the_crimson_depths/mixin/PlayerEntityMixin.java
@@ -1,15 +1,17 @@
 package arsenal.the_crimson_depths.mixin;
 
 import arsenal.the_crimson_depths.api.BreakShieldItem;
+import arsenal.the_crimson_depths.items.Custom.LanceItem;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
@@ -28,7 +30,21 @@ public abstract class PlayerEntityMixin {
         }
     }
 
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void crimson$pierceArmor(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
 
+        Entity attacker = source.getAttacker();
 
+        if (attacker instanceof PlayerEntity player) {
 
+            if (player.getMainHandStack().getItem() instanceof LanceItem) {
+
+                LivingEntity target = (LivingEntity)(Object)this;
+
+                target.setAbsorptionAmount(0);
+
+                target.timeUntilRegen = 0;
+            }
+        }
+    }
 }
